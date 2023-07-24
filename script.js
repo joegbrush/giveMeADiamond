e00aed62f275a655268945b7368588985fe1fd34
Joe Brush <joegbrush@gmail.com>
2023-07-24 10:30:49 -0500

    diamonds printing correctly , no spaces

---
+++ script.js
@@ -0,0 +1,14 @@
+function diamond(n){
+    let diamond = ''
+    let spaces = (n-1)/2
+    if(n === 1) {return diamond}
+    for(let i = 1; i < n; i++){
+     diamond +=  '*'.repeat(i) + '\n'
+    }
+    for(let i = n; i >= 1; i--){
+        diamond += '*'.repeat(i) + '\n'
+    }
+return diamond
+  }
+
+  console.log(diamond(5))
